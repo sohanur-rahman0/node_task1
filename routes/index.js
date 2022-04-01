cbcc2d46c7288052eda9a13f94e690df787ee147
sohanur-rahman0 <sajeebsrs@gmail.com>
2022-04-01 21:31:12 +0600

    product add and view product details by id done

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,60 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var path = require('path')
+var router = express.Router()
+const db = require('../config/db.config')
+const Product = require('../models/Product')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', async function (req, res, next) {
+  try {
+    const products = await Product.findAll()
+    // console.log(products)
+    res.render('index', { products, title: 'Products' })
+  } catch (error) {
+    console.log(error)
+  }
+})
 
-module.exports = router;
+router.get('/addProduct', async (req, res) => {
+  try {
+    res.render('addProduct', { title: 'Add Product' })
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+router.post('/addProduct', async (req, res) => {
+  try {
+    console.log(req.body)
+    if (!req.files || Object.keys(req.files).length === 0) {
+      res.redirect('/addProduct')
+    }
+
+    const image = req.files.image
+    const uploadPath = path.join(__dirname, '..', 'public', 'uploads', image.name)
+
+    await image.mv(uploadPath)
+
+    const { title, description, price } = req.body
+    await Product.create({
+      title,
+      description,
+      image: image.name,
+      price,
+    })
+    res.redirect('/')
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+router.get('/product/:id', async (req, res) => {
+  try {
+    const product = await Product.findByPk(req.params.id)
+    res.render('productDetails', { product, title: 'Product' })
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+module.exports = router
